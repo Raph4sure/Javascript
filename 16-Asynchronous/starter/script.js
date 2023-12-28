7346d4deca1b3b7eeca8703f063265129b44bbec
Raph4sure <raph4sure007@gmail.com>
2023-12-28 02:41:26 +0100

    Promisyfing Geolocation

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -192,3 +192,50 @@ btn.addEventListener('click', function () {
   // getCountryData('nigeria');
   getCountryData('Australia');
 });
+
+/* const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening 🔁');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💸');
+    } else {
+      reject(new Error('You lost your money 😥'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, (seconds = 1000));
+  });
+};
+
+wait(1).then(() => {
+  console.log('1 second passed');
+  return wait(1);
+}).then(() => {
+  console.log('2 second passed');
+  return wait(1);
+}).then(() => {
+  console.log('3 second passed');
+  return wait(1);
+}).then(() => {
+  console.log('4 seconds passed');
+  return wait(1);
+}); */
+
+
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+navigator.geoloctaion.getCurrentPosition(
+  position => console.log(position),
+  err => console.error(err)
+);
